a10869b02e9af495a096c9891110e868b39ff7f6
Omar Arja <omaralarja17@gmail.com>
2024-03-15 14:53:52 +0200

    Refactor auth module imports and providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [TypeOrmModule.forFeature([User]), PassportModule],
+  providers: [AuthService, LocalStrategy, UsersService],
   controllers: [AuthController],
 })
 export class AuthModule {}
