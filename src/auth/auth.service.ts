3d31bdb93f2f7d11f491486d9d4697c0dc0f7bc0
Omar Arja <omaralarja17@gmail.com>
2024-03-15 13:55:52 +0200

    Add user validation in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByUsername(username);
+    if (user && user.password === pass) {
+      return user;
+    }
+
+    return null;
+  }
+}
