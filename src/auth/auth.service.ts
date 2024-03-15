2ed3525e98e800598af15f03d72e446ba11e4646
Omar Arja <omaralarja17@gmail.com>
2024-03-15 13:57:25 +0200

    Refactor user validation in AuthService to exclude password from returned result

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,7 +8,9 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOneByUsername(username);
     if (user && user.password === pass) {
-      return user;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
     }
 
     return null;
