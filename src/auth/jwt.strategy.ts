c402b71b377acb68200c91ed27ad1d16d0399897
Omar Arja <omaralarja17@gmail.com>
2024-03-15 16:32:47 +0200

    Add user storage and update JWT strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { UserStorage } from 'src/shared/user.storage';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'SECRET',
@@ -12,6 +14,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    const user = await this.usersService.findOne(payload.sub);
+    if (!user) {
+      return null;
+    }
+
+    UserStorage.set(user);
+
     return {
       id: payload.sub,
       username: payload.username,
